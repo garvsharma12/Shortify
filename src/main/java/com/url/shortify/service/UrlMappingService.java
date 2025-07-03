9125c4d8636222a58e93c0d92f526847515e7cbc
garvsharma12 <garv.sharma200212@gmail.com>
2025-07-03 18:48:55 +0530

    Updated URL Redirect Functionality, with count management of the clicks

--- src/main/java/com/url/shortify/service/UrlMappingService.java
+++ src/main/java/com/url/shortify/service/UrlMappingService.java
@@ -110,6 +110,16 @@ public class UrlMappingService {
 
     public UrlMapping getOriginalUrl(String shortUrl) {
         UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if(urlMapping!=null){
+            urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+            urlMappingRepository.save(urlMapping);
+
+            //RecordingClickEvent
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
         return urlMapping;
     }
 }
